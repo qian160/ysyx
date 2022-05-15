fd047a84512d3a561f7c6c3825273b4141603769
nakashima <1601315694@qq.com>
2022-05-15 23:51:10 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard f42b05e1f03e369cfc6701efc8737cda972d2a24 keyboard: fix spelling mistake, fix #9

--- mux/obj_dir/test.d
+++
@@ -1,9 +0,0 @@
-test.o: ../test.cpp /home/s081/Downloads/verilator/include/verilated.h \
- /home/s081/Downloads/verilator/include/verilatedos.h \
- /home/s081/Downloads/verilator/include/verilated_config.h \
- /home/s081/Downloads/verilator/include/verilated_types.h \
- /home/s081/Downloads/verilator/include/verilated_funcs.h \
- /home/s081/Downloads/verilator/include/verilated_vcd_c.h \
- /home/s081/Downloads/verilator/include/verilated.h \
- /home/s081/Downloads/verilator/include/verilated_trace.h \
- /home/s081/Downloads/verilator/include/verilated_trace_defs.h Vmux41_2.h
--- mux/obj_dir/verilated.d
+++
@@ -1,9 +0,0 @@
-verilated.o: /home/s081/Downloads/verilator/include/verilated.cpp \
- /home/s081/Downloads/verilator/include/verilatedos.h \
- /home/s081/Downloads/verilator/include/verilated_imp.h \
- /home/s081/Downloads/verilator/include/verilated.h \
- /home/s081/Downloads/verilator/include/verilated_config.h \
- /home/s081/Downloads/verilator/include/verilated_types.h \
- /home/s081/Downloads/verilator/include/verilated_funcs.h \
- /home/s081/Downloads/verilator/include/verilated_syms.h \
- /home/s081/Downloads/verilator/include/verilated_sym_props.h
