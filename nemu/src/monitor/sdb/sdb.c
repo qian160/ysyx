67b22af943ea1fe7c6c91dfc665eb9ffabfd498f
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-21 22:11:39 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:11:39 up  2:46,  1 user,  load average: 0.53, 0.64, 0.56

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -114,6 +114,7 @@ void sdb_mainloop() {
     if (strcmp(cmd,"info") == 0)
     {
         char * addition = strtok(NULL," ");
+        cmd = strcat(cmd," ");
         cmd = strcat(cmd,addition);
         printf("%s\n",cmd);
     }
