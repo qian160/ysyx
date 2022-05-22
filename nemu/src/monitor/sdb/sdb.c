2ed150d68945f173f569556f8c42547f7ec10bea
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-22 16:16:19 +0800

    > compile NEMU ysyx_22040000 张三 Linux ubuntu 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:19 up  7:58,  1 user,  load average: 0.50, 0.43, 0.48

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -116,13 +116,13 @@ static int cmd_help(char *args) {
   if (arg == NULL) {
     /* no argument given */
     for (i = 0; i < NR_CMD; i ++) {
-      printf("%-s - %-s\n", cmd_table[i].name, cmd_table[i].description);
+      printf("\033[40;31m%8s - %-s\033[0m\n", cmd_table[i].name, cmd_table[i].description);
     }
   }
   else {
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("\033[40;31m%s - %s\033[0m\n", cmd_table[i].name, cmd_table[i].description);
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
         return 0;
       }
     }
