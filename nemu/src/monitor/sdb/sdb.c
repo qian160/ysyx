3d4821028cb4ede631d699099145dea85446f6bf
1919810 <tracer@ysyx.org>
2022-08-31 00:18:23 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:18:23 up 23:27,  1 user,  load average: 4.83, 4.05, 3.31

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -42,7 +42,7 @@ void examine_memory(int n, int64_t p){
   char cnt = 0;
   for (int i = 0 ; i < n ; i++)
   {
-    printf(ANSI_FMT("%02lx: ", ANSI_FG_YELLOW),pmem_read(p, 1));
+    printf(ANSI_FMT("%02lx  ", ANSI_FG_YELLOW),pmem_read(p, 1));
     //printf("\33[40;33m%02lx\33[0m  ",pmem_read(p,1));   //yellow
     p ++;
     cnt ++;
@@ -51,7 +51,7 @@ void examine_memory(int n, int64_t p){
       cnt = 0;
       printf("\n");
       //printf("\33[40;32m 0x%x\33[0m: ",p);
-        printf(ANSI_FMT(" 0x%lx ",ANSI_FG_MAGENTA), p);
+        printf(ANSI_FMT(" 0x%lx: ",ANSI_FG_MAGENTA), p);
     }
   }
   printf("\n");
