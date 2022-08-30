fdb914450d239234607f33f701b3ff34c9a1bcf0
1919810 <tracer@ysyx.org>
2022-08-30 15:25:58 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:58 up 17:37,  1 user,  load average: 0.27, 0.38, 0.35

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -4,7 +4,7 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
-
+#define test
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
   for(int i = 0 ; i < argc; i++)
     printf("%s\n",argv[i]);
 
-  printf("%d\n", MUXDEF(ITRACE_COND, 1, 0));
+  printf("%d\n", MUXDEF(test, 1, 0)); 
   /* Start engine. */
   engine_start();
 
