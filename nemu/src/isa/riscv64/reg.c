ee7f1ced8b7b3b8b8fff60a6db76a7e772e33606
1919810 <tracer@ysyx.org>
2022-08-31 00:45:59 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:45:59 up 23:55,  1 user,  load average: 2.35, 1.54, 1.81

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -1,7 +1,7 @@
 #include <isa.h>
 #include "local-include/reg.h"
 
-const char *regs[] = {
+const char *regs[] = {    //names
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
@@ -11,9 +11,7 @@ const char *regs[] = {
 void isa_reg_display() {
 
 	for (int i = 0; i <= 31; i ++) 
-    //-代表左对齐,31 = yellow
     printf(ANSI_FMT("%s\t\t0x%-16lx\t%-16ld\n", ANSI_FG_PINK), regs[i], gpr(i), gpr(i));
-  //printf("\033[40;38m%s\t\t%-16lx\t%-16ld\n\033[0m", regs[i],gpr(i),gpr(i));
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
