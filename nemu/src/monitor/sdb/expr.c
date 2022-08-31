c28df718cbf65c070f2aefed8280094e98fbdc9e
1919810 <tracer@ysyx.org>
2022-08-31 16:01:05 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:05 up 1 day,  6:31,  1 user,  load average: 1.49, 0.99, 0.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -96,7 +96,7 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
-static bool make_token(char *e) {
+/*static*/ bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
