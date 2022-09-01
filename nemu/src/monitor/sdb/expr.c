5ed46636670644d1a569c0657ce9ee5eaaad0acc
1919810 <tracer@ysyx.org>
2022-09-01 16:29:09 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:29:09 up 1 day, 13:59,  1 user,  load average: 1.01, 0.64, 0.64

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -126,7 +126,9 @@ bool check_parentheses(int p, int q){   //scan the array and use a stack
   printf(ANSI_FMT("start check....the substr is:\n",ANSI_FG_MAGENTA));
   for(int k = p; k < q; k++)
     printf("%s\t", tokens[k].str);
-  printf("\n");
+    printf("\n");
+  printf(ANSI_FMT("token[p] = %s\n",ANSI_FG_YELLOW),tokens[p]);
+  printf(ANSI_FMT("token[q] = %s\n",ANSI_FG_YELLOW),tokens[q]);
   //if surrounded by a pair of parentheses, just throw it away
   if(tokens[p].type == TK_LEFT && tokens[q].type == TK_RIGHT){
     printf("parentheses pair found. old array:\n");
