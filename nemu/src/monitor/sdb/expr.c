5d4d2a576bed8c5e0cd7adaec0e555fff76bbc73
1919810 <tracer@ysyx.org>
2022-09-01 15:37:13 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:37:13 up 1 day, 13:21,  1 user,  load average: 1.65, 1.09, 0.93

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -125,10 +125,17 @@ bool check_parentheses(int p, int q){   //scan the array and use a stack
   S.top = 0;    ///reset the stack
   //if surrounded by a pair of parentheses, just throw it away
   if(tokens[p].type == TK_LEFT && tokens[q].type == TK_RIGHT){
+    printf("parentheses pair found. old array:\n")
+    for(int j = p; j < q; j++)
+      printf("%s\t", tokens[j].str);
     for(int i = p; i < q - 2; i++){
       tokens[i] = tokens[i+1];
     }
     nr_token -= 2;
+    printf("\nnew array:\n");
+    for(int k = 0; k < nr_token; k++)
+      printf("%s\t", tokens[k].str);
+
   }
   for(; p < q; p++){
     char type = tokens[p].type;
