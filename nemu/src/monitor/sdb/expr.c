5ad8a8d8be9c3d222f135fa85601f8717e839af1
1919810 <tracer@ysyx.org>
2022-09-02 16:07:26 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:07:26 up 11:32,  1 user,  load average: 0.84, 0.71, 0.75

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -191,7 +191,8 @@ int find_prime_idx(int p, int q)    //the prime opt should have low privilege
   PS.top = 0;
   Log("from %d to %d", p, q);
   while(tokens[p].type == TK_LEFT && tokens[q].type == TK_RIGHT){
-    p++;q--;
+    p++;
+    q--;
   }
   for(; p <= q; p++ ){
     int type = tokens[p].type;
@@ -244,7 +245,7 @@ static bool make_token(char *e) {
         //    i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         //produce the token string and copy it to the array
-
+        //empty token could just be thrown away
         if(rules[i].token_type != TK_NOTYPE){
             char * token = (char *)malloc(substr_len + 1);
             for(int t = 0; t < substr_len; t++)
@@ -310,17 +311,13 @@ word_t calculate(int p, int q, bool * success){
   }
   else {
     char * removed1 = (char *)malloc(1);   //the number of pair of parentheses removed
-    *removed1 = 0;
     char * removed2 = (char *)malloc(1);
+    *removed1 = 0;
     *removed2 = 0;
     /*it's hard to decide which function to call first(find vs check)
       if we call find first, the left && right 2 substrs will be divided and this may affect the check
       but if we call check first, since not all the left-most parentheses match with the right-most ones, it may not remove the tokens correctly
     */
-    while(tokens[p].type == TK_LEFT && tokens[q].type == TK_RIGHT){
-      tokens[p++].type = TK_NOTYPE;
-      tokens[q--].type = TK_NOTYPE;
-    }
     int prime = find_prime_idx(p, q);
     type = tokens[prime].type;
     //some preprocess must be done before the substr's check...
@@ -328,7 +325,7 @@ word_t calculate(int p, int q, bool * success){
     bool checkRight = check_parentheses(prime + 1, q, removed2);
     //Log("p = %d, q = %d, prime = %d, left check: %d, right check: %d\n",p, q, prime, checkLeft, checkRight);
     if(!checkLeft || !checkRight){
-      if(check_parentheses(p, q, removed1)){}   //also acceptable?
+      if(check_parentheses(p, q, removed1)){}   //maybe also acceptable?
       else if(!(type == TK_DECNUM || type == TK_HEXNUM)){
         printf(ANSI_FMT("illegal expression...maybe bugs also\n",ANSI_FG_RED));
         return 0;
