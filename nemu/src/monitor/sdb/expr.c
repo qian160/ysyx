afafc4468f06b4678f759401a748283a53948e10
1919810 <tracer@ysyx.org>
2022-09-03 13:49:32 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:49:32 up 21:48,  1 user,  load average: 1.63, 1.07, 0.89

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -381,22 +381,7 @@ word_t calculate(int p, int q){
       case(COND_AND): return P1 && P2;
       case(NOTEQAL):  return P1 != P2;
       case(EQUAL):    return P1 == P2;
-      //well, we still need this... just consider expressions like a singal number such as: 1
-      default: Assert(0, "bad token\n");
-      /*
-      {
-        word_t result;
-        if(type == DECNUM){
-          sscanf(tokens[prime].str, "%ld", &result);
-        }
-        else if(type == HEXNUM){
-          sscanf(tokens[prime].str, "%lx", &result);
-        }
-        return result;
-      }
-      */
-      //Assert(0, "bad type: hope this would not happen.......%d\n",type);
-    }
+      default: Assert(0, "bad op type: %d\n", type);
   }
   return 0; //will not be execuated..
 }
