1eb5246bd2c771f713b4d20047ff3e99f552ec9f
1919810 <tracer@ysyx.org>
2022-09-04 12:49:12 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:49:12 up 1 day, 11:31,  1 user,  load average: 2.06, 1.42, 1.11

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -4,7 +4,7 @@
 #include <common.h>
 
 static inline int check_reg_idx(int idx) {
-  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 32));
+  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx <= 32));  //add pc, for debug...
   return idx;
 }
 
