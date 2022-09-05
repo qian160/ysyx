591cf4eecd1fb2288dcc25aedfabd86b0aea39a7
1919810 <tracer@ysyx.org>
2022-09-05 16:22:11 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:11 up 2 days,  2:28,  1 user,  load average: 1.10, 0.93, 0.87

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -5,13 +5,13 @@ const char *regs[] = {    //names.. add $ prefix to make regex match easier
   "$0",   "$ra", "$sp",   "$gp",  "$tp",  "$t0",  "$t1",  "$t2",
   "$s0",  "$s1", "$a0",   "$a1",  "$a2",  "$a3",  "$a4",  "$a5",
   "$a6",  "$a7", "$s2",   "$s3",  "$s4",  "$s5",  "$s6",  "$s7",
-  "$s8",  "$s9", "$s10",  "$s11", "$t3",  "$t4",  "$t5",  "$t6", "$pc", //pc is for debug use,
+  "$s8",  "$s9", "$s10",  "$s11", "$t3",  "$t4",  "$t5",  "$t6", 
 };
 
 void isa_reg_display() {
 
-	for (int i = 0; i <= 31; i ++) 
-    printf(ANSI_FMT("%s\t\t0x%-16lx\t%-16ld\n", ANSI_FG_PINK), regs[i], gpr(i), gpr(i));
+	for (int i = 0; i < 31; i += 2) 
+    printf(ANSI_FMT("%s:\t0x%-16lx\t%s:\t0x%-16ld\n", ANSI_FG_PINK), regs[i], gpr(i),regs[i+1], gpr(i+1));
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
