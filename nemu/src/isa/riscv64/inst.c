3527a3c76efe4647177641cd64be8cce713607bb
1919810 <tracer@ysyx.org>
2022-09-05 18:51:30 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:51:30 up 2 days,  4:15,  1 user,  load average: 0.48, 0.81, 0.89

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -8,7 +8,7 @@
 #define Mw vaddr_write  //memory write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
   TYPE_N, // none
 };
 
@@ -22,17 +22,25 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immJ(uint32_t i) { return SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1 ), 20); }
 
+//src1 and src2 are not source operands, they are just used to communicate with outside
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t inst = s->isa.inst.val;
   int rd  = BITS(inst, 11, 7);
   int rs1 = BITS(inst, 19, 15);
   int rs2 = BITS(inst, 24, 20);
+  word_t JAL_TARGET = s -> pc + 4;
+  word_t imm_J = immJ(inst);
+  word_t imm_I = immI(inst);
+  word_t imm_U = immU(inst);
+  word_t imm_S = immS(inst);
   destR(rd);
   switch (type) {
     case TYPE_I: src1R(rs1);     src2I(immI(inst)); break;
     case TYPE_U: src1I(immU(inst)); break;
     case TYPE_S: destI(immS(inst)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(JAL_TARGET);  src2I(immJ(inst));break;
   }
 }
 
@@ -55,9 +63,11 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 011 ????? 0000011",  ld      , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 000 ????? 0010011",  addi    , I, R(dest) = src1 + src2);
   INSTPAT("??????? ????? ????? 011 ????? 0100011",  sd      , S, Mw(src1 + dest, 8, src2));
+  INSTPAT("??????? ????? ????? ??? ????? 1101111",  jal     , J, R(dest) = src1, s->dnpc = src2);
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak  , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", invalid , N, INV(s->pc));
   INSTPAT_END();
+
 
   R(0) = 0; // reset $zero to 0
   return 0;
