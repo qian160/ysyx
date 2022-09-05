3dda6e61f546a55c98ff11d5e765cc5dea3a1625
1919810 <tracer@ysyx.org>
2022-09-05 19:00:36 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:00:36 up 2 days,  4:24,  1 user,  load average: 1.71, 0.99, 0.91

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -39,7 +39,7 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
   switch (type) {
     case TYPE_I: src1R(rs1);     src2I(imm_I); break;
     case TYPE_U: src1I(imm_U); break;
-    case TYPE_S: destI(imm_S); src1R(rs1); src2R(rs2); break;
+    case TYPE_S: destI(immS(inst)); src1R(rs1); src2R(rs2); break;
     case TYPE_J: src1I(JAL_TARGET);  src2I(imm_J);break;
   }
 }
