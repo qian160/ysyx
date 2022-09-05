cab57424e32363472a8e8c23437725df1bc1d72f
1919810 <tracer@ysyx.org>
2022-09-05 11:42:52 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:42:52 up 1 day, 23:14,  1 user,  load average: 1.02, 1.18, 1.00

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -26,7 +26,7 @@ static inline void pattern_decode(const char *str, int len,
         __key   = (__key << 1)  | (c == '1');   //extract all the high bits
         __mask  = (__mask << 1) | (c != '?');   //extract all the valid bits,(0/1, not ?)
         __shift = (c == '?' ? __shift + 1: 0);  //the number of continuous ? from right to left
-        printf("\ni = %d, c = %c key = %lx, mask = %lx, shift = %lx\n", i, c, __key, __mask, __shift);
+        //printf("\ni = %d, c = %c key = %lx, mask = %lx, shift = %lx\n", i, c, __key, __mask, __shift);
     }
   }
   //as an example, when str = "10?", key = 0b100, maks = 0x110, shift = 1
@@ -36,7 +36,7 @@ static inline void pattern_decode(const char *str, int len,
   *key = __key >> __shift;
   *mask = __mask >> __shift;
   *shift = __shift;
-  printf("\n\n\nstr = %s\nkey = %lx, mask = %lx, shift = %lx\n", str, *key, *mask, *shift);
+  //printf("\n\n\nstr = %s\nkey = %lx, mask = %lx, shift = %lx\n", str, *key, *mask, *shift);
 
 }
 
