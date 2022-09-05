4a686c60cc625c067029558bcee17cb899f0b727
1919810 <tracer@ysyx.org>
2022-09-05 15:50:39 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:50:39 up 2 days,  1:57,  1 user,  load average: 0.58, 0.95, 0.87

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -69,8 +69,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'i': img_file = optarg; return 0;
-      //case  1 : img_file = optarg; return 0;  //case 1?? 
+      case  1 : img_file = optarg; return 0;  //case 1?? 
       default:
         printf("unknown opt %s\n\n", optarg);
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
