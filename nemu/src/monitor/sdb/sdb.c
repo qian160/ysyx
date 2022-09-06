956d94c503c34c8c2f325f4de03944c69daa7a53
1919810 <tracer@ysyx.org>
2022-09-06 19:29:54 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:29:54 up 2 days, 16:41,  3 users,  load average: 2.02, 1.00, 1.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -82,7 +82,6 @@ void sdb_mainloop() {   //get command
   {
     int n = atoi(CONFIG_AUTO_DISASM_NEXT);
     printf(ANSI_FMT("\nnext %d instruction(s) is:\n", ANSI_FG_YELLOW), n);
-    char * s  = CONFIG_AUTO_DISASM_NEXT;
     char *buf = (char *) malloc(30);
     memset(buf, 0, 30);
     char * arg = strcat(strcat(strcat(buf, " "), CONFIG_AUTO_DISASM_NEXT), " $pc");
