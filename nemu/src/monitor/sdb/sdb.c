6848433b1998b91a006bd926f1b3ff8dac804fda
1919810 <tracer@ysyx.org>
2022-09-06 15:01:34 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:01:34 up 2 days, 14:41,  3 users,  load average: 1.03, 0.99, 0.91

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -69,7 +69,8 @@ void sdb_mainloop() {   //get command
 
     int i;
     for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(cmd, cmd_table[i].short_name) == 0 || streq(cmd, cmd_table[i].full_name)) {
+      if (streq(cmd, cmd_table[i].short_name) || streq(cmd, cmd_table[i].full_name)) {
+        Log("\nargs = %s\n", args);
         if (cmd_table[i].handler(args) < 0) { return; } //error when running
         break;
       }
