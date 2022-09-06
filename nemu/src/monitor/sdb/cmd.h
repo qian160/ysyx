913f3f24df380f68039c3f8cdb085e3544386c1c
1919810 <tracer@ysyx.org>
2022-09-06 11:25:38 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:38 up 2 days, 11:36,  1 user,  load average: 0.97, 1.15, 1.00

--- nemu/src/monitor/sdb/cmd.h
+++ nemu/src/monitor/sdb/cmd.h
@@ -198,7 +198,7 @@ static int cmd_d(char * e){
         //name
         disassemble(p, buf + sizeof(buf) - p, pc, (uint8_t *)&inst, 4);
         //full inst is ready
-        printf(ANSI_FMT(buf, ANSI_FG_CYAN));
+        puts(buf);
         pc += 4;        
     }
 
