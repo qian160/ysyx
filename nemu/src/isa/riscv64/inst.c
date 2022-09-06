fa2679ff3389dfb45861c57d07ee9b5373016550
1919810 <tracer@ysyx.org>
2022-09-06 20:03:41 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:03:41 up 2 days, 17:15,  3 users,  load average: 0.94, 1.08, 1.15

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -91,13 +91,13 @@ static int decode_exec(Decode *D) {
   decode_operand(D, &dest, &src1, &src2, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
   IFDEF(CONFIG_SHOW_DECODE_INFORMATION,  \
-  puts(ANSI_FMT("\nInformation about the just execuated instruction:", ANSI_FG_YELLOW));\
+  puts(ANSI_FMT("\nInformation about the just execuated instruction:", ANSI_FG_GREEN));\
   char buf[30];\
   disassemble(buf, sizeof(buf), D -> pc, (uint8_t *)(&D -> inst), 4);\
-  printf(ANSI_FMT("type-%c: \t%s  \noperand1 = 0x%-16lx, operand2 = 0x%-16lx \n", ANSI_FG_YELLOW),tp[TYPE_##type], buf, src1, src2);\
+  printf(ANSI_FMT("type-%c: \t%s  \noperand1 = 0x%-16lx, operand2 = 0x%-16lx \n", ANSI_FG_GREEN),tp[TYPE_##type], buf, src1, src2);\
   switch(TYPE_##type){  \
     case(TYPE_I):case(TYPE_R):case(TYPE_U):\
-      printf(ANSI_FMT("this set %s to be 0x%lx\n", ANSI_FG_YELLOW), reg_name(dest, 64), R(dest)); break;\
+      printf(ANSI_FMT("this set %s to be 0x%lx\n", ANSI_FG_GREEN), reg_name(dest, 64), R(dest)); break;\
     case(TYPE_B):case(TYPE_J):\
       if( src1 == 0){  \
         printf(ANSI_FMT("branch/jump not taken\n",  ANSI_FG_YELLOW)); break;}\
