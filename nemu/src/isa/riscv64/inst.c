9dde0b64c8706f33ca975d90699951099f9b8e6a
1919810 <tracer@ysyx.org>
2022-09-07 13:34:45 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:34:45 up 2 days, 22:50,  3 users,  load average: 1.34, 1.02, 0.83

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -107,7 +107,7 @@ static int decode_exec(Decode *D) {
       printf(ANSI_FMT("load a value 0x%lx from address: 0x%lx\n", ANSI_FG_YELLOW), Mr(src1 + src2, L_width(fct3)), src1 + src2); \
     }  \
     else if(D->decInfo.is_jalr){\
-      printf(ANSI_FMT("jalr, set %s = 0x%lx, new PC at 0x%lx", ANSI_FG_YELLOW), reg_name(dest), src1, src2);\
+      printf(ANSI_FMT("jalr, set %s = 0x%lx, new PC at 0x%lx\n", ANSI_FG_YELLOW), reg_name(dest), src1, src2);\
     }\
     else  {\
       printf(ANSI_FMT("set %s to be 0x%lx\n", ANSI_FG_GREEN), reg_name(dest), R(dest)); \
@@ -118,11 +118,11 @@ static int decode_exec(Decode *D) {
         printf(ANSI_FMT("branch is not taken\n",  ANSI_FG_YELLOW)); \
       }\
       else {\
-        printf(ANSI_FMT("branch is taken, new PC at 0x%lx", ANSI_FG_YELLOW), src2); \
+        printf(ANSI_FMT("branch is taken, new PC at 0x%lx\n", ANSI_FG_YELLOW), src2); \
       }\
       break;\
     case(TYPE_J):\
-      printf(ANSI_FMT("jal, set %s = 0x%lx, new PC at 0x%lx", ANSI_FG_YELLOW), reg_name(dest), src1, src2);\
+      printf(ANSI_FMT("jal, set %s = 0x%lx, new PC at 0x%lx\n", ANSI_FG_YELLOW), reg_name(dest), src1, src2);\
       break;\
     case(TYPE_S):{\
       printf(ANSI_FMT("store a value 0x%llx to address 0x%lx\n", ANSI_FG_YELLOW), src2 | BITMASK(S_width(fct3) << 3), src1);\
