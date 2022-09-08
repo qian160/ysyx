836094515466e1ea534cda4524c59996b028299b
1919810 <tracer@ysyx.org>
2022-09-08 18:14:01 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:14:01 up 3 days, 15:50,  3 users,  load average: 1.21, 0.82, 0.66

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -5,16 +5,25 @@
 // but it is ok since we do not access the array directly
 //a memcpy will overcover this if a img file is used
 static const uint32_t img [] = {             //offset
-  0x00000297,  // auipc t0,0                  /*0*/
-  0xfff00093,  // li x1, -1
-  0x80000093,  // li x1, -2048                /*4*/
-  0x00100113,  // li x2, 1                    /*8*/
-  0x003081bb,  // addw x3, x1, x2             /*12*/
-  0x0052be23,  // sd t0, 28(t0)               /*16*/
-  0x01c2be03,  // ld a0, 28(t0)               /*20*/
-  0x00100073,  // ebreak (used as nemu_trap)  /*24*/
-  0x41919810,  // some data                   /*28*/
-  0x00011451,                                 /*32*/
+
+0xfff00093,
+0x00200113,
+0x002081bb,
+0x00600093,
+0xfff00113,
+0x002081bb,
+
+/*
+  0x00000297,  // auipc t0,0                  /*0
+  0x80000093,  // li x1, -2048                /*4
+  0x00100113,  // li x2, 1                    /*8
+  0x003081bb,  // addw x3, x1, x2             /*12
+  0x0052be23,  // sd t0, 28(t0)               /*16
+  0x01c2be03,  // ld a0, 28(t0)               /*20
+  0x00100073,  // ebreak (used as nemu_trap)  /*24
+  0x41919810,  // some data                   /*28
+  0x00011451,                                 /*32
+*/
 };
 
 static void restart() {
