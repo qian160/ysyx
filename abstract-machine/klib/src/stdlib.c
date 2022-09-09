39017e846a1d9065cd3bd70b1f3070a8d6a54645
1919810 <tracer@ysyx.org>
2022-09-09 23:02:16 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:02:16 up 4 days,  7:53,  3 users,  load average: 1.11, 0.81, 0.85

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -35,7 +35,7 @@ void *malloc(size_t size) { //size: bytes
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  //panic("Not implemented");
 #endif
   void * addr = heap.start;
   heap.start += size*8;
