cdbf49fb7c8ff73769c6a32c0b8ef3320d4e5141
1919810 <tracer@ysyx.org>
2022-09-09 14:26:40 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:26:40 up 4 days, 50 min,  3 users,  load average: 2.95, 2.47, 1.69

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -2,43 +2,95 @@
 #include <klib-macros.h>
 #include <stdint.h>
 
+#include<stdio.h>
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
   size_t len = 0;
+  if(!s) return 0;
   while(*s++)len++;
   return len;
+  // \0 is not counted
   //panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  if(strlen(dst) < strlen(src))
+    printf("dest < src, can't do this\n");
+  else{
+    while(*src){
+      *dst++ = *src++;
+    }
+  }
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  if(n > strlen(src))
+    printf("too many bytes\n");
+  else{
+    while(n--){
+      *dst++ = *src++;
+    }
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  if(!dst || !src){
+    printf("null\n");
+    return NULL;
+  }                                                                                 
+  // make ptr point to the end of the destination string
+  char* ptr = dst + strlen(dst);
+
+  // appends characters of the source to the destination string
+  while (*src != '\0') {
+      *ptr++ = *src++;
+  }   
+  // null terminate destination string
+  *ptr = '\0';
+
+  return dst;
+
 }
 
 int strcmp(const char *s1, const char *s2) {
+  if(!s1 || !s2 || strlen(s1) != strlen(s2)){
+    return 114514;   //non-zero for failure
+  }
+  while(*(s1) == *(s2)){
+    s1++;s2++;
+  }
+  if(*s1 == '\0') return 0;
+  return 1;
   panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  if(n == 0) return 1;
+  if(n > strlen(s1) || n > strlen(s2))
+    return -1;
+  while(n--){
+    if(*s1++ != *s2++)return -1;
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  //seems that n can be larger than sizeof(s)
+  if (!s) return NULL;
+    while(n--){
+      *(char *)s++ = c;
+    }
+  return s;
 }
-
+//can overlap
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
 }
-//overlap
+//can not overlap. UB
 void *memcpy(void *out, const void *in, size_t n) {
   panic("Not implemented");
 }
--- nemu/a.c
+++ nemu/a.c
@@ -1,18 +1,30 @@
 #include<stdio.h>
 #include<stdint.h>
 
-typedef uint64_t word_t;
+char *my_strcat(char *dst, const char *src) {
+  if(!dst || !src){
+    printf("null\n");
+    return NULL;
+  }
+    // make `ptr` point to the end of the destination string
+    char* ptr = dst + strlen(dst);
+ 
+    // appends characters of the source to the destination string
+    while (*src != '\0') {
+        *ptr++ = *src++;
+    }
+    // null terminate destination string
+    *ptr = '\0';
+ 
+    // the destination is returned by standard `strcat()`
+    return dst;
+}
 
-#define BITMASK(bits) ((1ull << (bits)) - 1)
-
-#define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1)) // similar to x[hi:lo] in verilog
-
-#define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
-
-
-static word_t immJ(uint32_t i) { return SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1 | 0 ), 21); }
-
-int main(){
-	word_t t  = immJ(0xf75fffff);
-	printf("%lx\n", t);
+int main()
+{
+	char * s1 = "hello";
+	char * s2 = "world";
+	char * str = (char*)calloc(100, 1);
+	printf("%s\n", my_strcat(str, "hello"));
+	printf("%s\n", my_strcat(s1, s2));
 }
