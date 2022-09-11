77f10155148e62252a61c9b38bf6c11d04cdbfbd
1919810 <tracer@ysyx.org>
2022-09-11 18:17:00 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:17:00 up 5 days,  5:42,  3 users,  load average: 5.74, 3.26, 1.80

--- nemu/src/trace.c
+++ nemu/src/trace.c
@@ -80,9 +80,10 @@ void show_ftrace(){
 
 int depth = 0;
 
-void update_ftrace(bool is_call, word_t addr, const char * name, int depth){
+void update_ftrace(bool is_call, word_t addr, const char * name, int d){
+    Log("\n%d\t%lx\t%s\t%d\n", is_call, addr, name, d);
     int cnt = ftrace.cnt;
-    ftrace.trace[cnt].depth   = depth;
+    ftrace.trace[cnt].depth   = d;
     ftrace.trace[cnt].is_call = is_call;
     strcpy(ftrace.trace[cnt].name, name);
     ftrace.trace[cnt].address = addr;
