1b4cdce95bd2ad68a7d07a9464052c6150122575
1919810 <tracer@ysyx.org>
2022-09-11 18:39:04 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:39:04 up 5 days,  6:04,  3 users,  load average: 0.37, 0.62, 1.07

--- nemu/src/trace.c
+++ nemu/src/trace.c
@@ -72,7 +72,7 @@ void show_ftrace(){
         char * s = f.is_call ? "call" : "ret ";
         for(char j = 0; j < f.depth; j++)
             printf("  ");
-        printf(ANSI_FMT("%s %s <0x%lx>, depth = %d\n", ANSI_FG_YELLOW), s, f.name, f.address, f.depth);
+        printf(ANSI_FMT("%s %s <0x%lx>\n", ANSI_FG_YELLOW), s, f.name, f.address);
     }
     putchar('\n');
 }
