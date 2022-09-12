2e8158a1311c2104d150c04512d91b772bb297a4
1919810 <tracer@ysyx.org>
2022-09-12 21:57:01 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:57:01 up 6 days, 53 min,  3 users,  load average: 0.90, 0.48, 0.25

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -30,20 +30,24 @@ static word_t immJ(uint32_t i) { return SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 
 static word_t immB(uint32_t i) { return SEXT((BITS(i, 31, 31) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | 0, 13);}
 
 #ifdef CONFIG_FTRACE_ENABLE
-  void _ftrace(bool is_ret, bool flag, word_t pc, word_t addr, const char * name, int type){
+  void _ftrace(Decode * D){
     //is_ret need to be improved, jal could also ret
+    char * name = getFuncName(D->decInfo.target);
     if(!name) return;   //not a function call or ret
-    switch(type){
+    word_t addr = D->decInfo.target;
+    switch(D->decInfo.type){
       case(TYPE_B):
-        if(flag) 
+        if(D->decInfo.branch_taken) 
           //update_ftrace(1, addr, pc, name, depth);
         break;
-      case(TYPE_I):
-        if(is_ret)
-          update_ftrace(1, addr, pc, name, depth);
+      case(TYPE_I):   //jalr, and other normal insts
+        if(D->decInfo.is_jalr)
+          update_ftrace(1, addr, D->pc, name, depth);
+        else
+
         break;
       case(TYPE_J):
-        update_ftrace(is_ret, addr, pc, name, depth);
+        update_ftrace(D->decInfo.is_ret, addr, D->pc, name, depth);
         break;
     }
   }
@@ -99,7 +103,7 @@ printf(ANSI_FMT(" --------------------------------------------------------------
       IFDEF(CONFIG_MTRACE_ENABLE, update_mringbuf(1, address, loadVal, dest));
     }
     else if(D->decInfo.is_jalr){
-      printf(ANSI_FMT("| jalr, set %s = 0x%-lx, new PC at 0x%lx. %s's bits are: |\n", ANSI_FG_YELLOW), reg_name(dest), src1, src2, reg_name(dest));
+      printf(ANSI_FMT("| jalr, set %s = 0x%-lx, new PC at 0x%lx. %s's bits are: \t\t|\n", ANSI_FG_YELLOW), reg_name(dest), src1, src2, reg_name(dest));
       show_bits_fmt(src1);
       /*IFDEF(CONFIG_FTRACE_ENABLE,
         if(dest == 0) update_ftrace(0, src2, "dont know", depth);
@@ -156,14 +160,20 @@ static void decode_operand(Decode * D, word_t *dest, word_t *src1, word_t *src2,
   //Log("\nimmJ = 0x%lx\nimmI = 0x%lx\nimmB = 0x%lx\nimmS = 0x%lx\n", immJ(inst), immI(inst), immB(inst), immS(inst));
   word_t BRANCH_TARGET  = immB(inst) + pc;
   word_t storeAddr      = immS(inst) + rs1Val;
+  D->decInfo.src1 = src1;
+  D->decInfo.src2 = src2;
+  D->decInfo.rd   = rd;
+  D->decInfo.target = 0;
+  D->decInfo.type = type;
+  D->decInfo.is_ret = 0;
   destR(rd);
   switch (type) {
     case TYPE_R: src1I(rs1Val);       src2I(rs2Val);    break;
     case TYPE_S: src1I(storeAddr);    src2R(rs2);       break;
-    case TYPE_J: src1I(pc_Plus4);     src2I(JAL_TARGET);break;
+    case TYPE_J: src1I(pc_Plus4);     src2I(JAL_TARGET);  D->decInfo.target = JAL_TARGET; D->decInfo.is_ret = (rd == 0); break;
     case TYPE_I: {
       if(D -> decInfo.is_jalr){ //jalr is I type, which is special
-          src1I(pc_Plus4);    src2I(JALR_TARGET); break;
+          src1I(pc_Plus4);    src2I(JALR_TARGET);  D->decInfo.target = JALR_TARGET;   D->decInfo.is_ret = (rd == 0 && rs1 == 1);  break;
       }
       else{
           src1R(rs1);         src2I(immI(inst));  break;
@@ -180,13 +190,14 @@ static void decode_operand(Decode * D, word_t *dest, word_t *src1, word_t *src2,
     }
     case TYPE_B: {
       src2I(BRANCH_TARGET);
+      D->decInfo.target = BRANCH_TARGET;
         switch (D -> decInfo.funct3){  //use src1 as a flag, src2 = branch_target
-        case beq_funct3:  src1I(rs1Val == rs2Val);  break;
-        case bne_funct3:  src1I(rs1Val ^  rs2Val);  break;
-        case blt_funct3:  src1I((sword_t)rs1Val <  (sword_t)rs2Val);  break;
-        case bge_funct3:  src1I((sword_t)rs1Val >= (sword_t)rs2Val);  break;
-        case bltu_funct3: src1I(rs1Val <  rs2Val);  break;
-        case bgeu_funct3: src1I(rs1Val >= rs2Val);  break;
+        case beq_funct3:  src1I(rs1Val == rs2Val);  D->decInfo.branch_taken = (rs1Val == rs1Val);  break;
+        case bne_funct3:  src1I(rs1Val ^  rs2Val);  D->decInfo.branch_taken = (rs1Val ^  rs1Val);  break;
+        case blt_funct3:  src1I((sword_t)rs1Val <  (sword_t)rs2Val);  D->decInfo.branch_taken = ((sword_t)rs1Val <  (sword_t)rs1Val); break;
+        case bge_funct3:  src1I((sword_t)rs1Val >= (sword_t)rs2Val);  D->decInfo.branch_taken = ((sword_t)rs1Val >= (sword_t)rs1Val); break;
+        case bltu_funct3: src1I(rs1Val <  rs2Val);  D->decInfo.branch_taken = (rs1Val <  rs1Val);   break;
+        case bgeu_funct3: src1I(rs1Val >= rs2Val);  D->decInfo.branch_taken = (rs1Val >= rs1Val);   break;
         }
     }
   }
@@ -204,7 +215,7 @@ static int decode_exec(Decode *D) {
   IFDEF(CONFIG_SHOW_DECODE_INFORMATION, show_decode(D, src1, src2, dest, TYPE_##type));\
   \
   IFDEF(CONFIG_FTRACE_ENABLE, \
-    bool ret = ((TYPE_##type == TYPE_I && D -> decInfo.is_jalr) || TYPE_##type == TYPE_J)  && dest == 0; _ftrace(ret, src1, D -> pc,  src2, getFuncName(src2), TYPE_##type ));\
+    bool ret = (((TYPE_##type == TYPE_I && D -> decInfo.is_jalr) || TYPE_##type == TYPE_J)  && dest == 0); _ftrace(D));\
     printf("%d\n", ret);\
 }
 
