5e8db933ae7e1f7e699cd7c8b77a625fbad12e92
1919810 <tracer@ysyx.org>
2022-09-12 15:14:54 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:54 up 5 days, 19:32,  3 users,  load average: 1.04, 1.11, 1.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -124,7 +124,18 @@ void cpu_exec(uint64_t n) {
 
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break; //n instructions have been execuated, time to stop and wait for new cmd
-
+    
+    case NEMU_END: case NEMU_ABORT:
+      Log("nemu: %s at pc = " FMT_WORD,
+          (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
+            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
+            ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
+          nemu_state.halt_pc);      
+          IFDEF(CONFIG_ITRACE_ENABLE, show_itrace());
+          IFDEF(CONFIG_MTRACE_ENABLE, show_mtrace());
+          IFDEF(CONFIG_FTRACE_ENABLE, show_ftrace());
+    
+/*
     case NEMU_END: 
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
@@ -135,10 +146,8 @@ void cpu_exec(uint64_t n) {
     //at first I want to use red, but it makes your eyes uncomfortabe
       Log("nemu: %s at pc = " FMT_WORD,
           ANSI_FMT("ABORT", ANSI_FG_RED), nemu_state.halt_pc);
+*/
 
-      IFDEF(CONFIG_ITRACE_ENABLE, show_itrace());
-      IFDEF(CONFIG_MTRACE_ENABLE, show_mtrace());
-      IFDEF(CONFIG_FTRACE_ENABLE, show_ftrace());
       // fall through
     case NEMU_QUIT: statistic();
   }
