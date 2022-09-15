710727acd842a5567edd51ef56c1cb2c8ed0283d
1919810 <tracer@ysyx.org>
2022-09-15 14:33:20 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:33:20 up 7 days,  4:00,  3 users,  load average: 0.74, 1.00, 1.01

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -6,6 +6,7 @@
 #include <utils.h>
 #include <difftest-def.h>
 
+//pointers to functions. And these functions will be found in the .so file
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -2,8 +2,15 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+//CPU_state and suruct diff_context has same fields
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  CPU_state temp;
+  ref_difftest_regcpy(&temp, DIFFTEST_TO_DUT);
+  for(int i = 0; i < 32; i++){
+    if(temp.gpr[i] ^ ref_r->gpr[i]) return false;
+  }
+  if(temp.pc  ^ ref_r -> pc)  return false;
+  return true;
 }
 
 void isa_difftest_attach() {
