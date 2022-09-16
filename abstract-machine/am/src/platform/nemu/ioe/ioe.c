705f4d9f4408e9ed26c6a5f0d7eafde450733e07
1919810 <tracer@ysyx.org>
2022-09-16 16:01:49 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:49 up 7 days, 16:25,  3 users,  load average: 0.64, 0.65, 0.83

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -4,6 +4,7 @@
 void __am_timer_init();
 void __am_gpu_init();
 void __am_audio_init();
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
