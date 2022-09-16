389cfb7320e20150bb64e313184a4a7d42d3a832
1919810 <tracer@ysyx.org>
2022-09-16 14:02:15 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:02:15 up 7 days, 14:26,  3 users,  load average: 1.11, 0.87, 0.86

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -155,7 +155,7 @@ static void decode_operand(Decode * D, word_t *dest, word_t *src1, word_t *src2,
     case TYPE_I: {
       if(D -> decInfo.is_jalr){ //jalr is I type, which is special
           Log("is jalr\n");
-          src1I(pc_Plus4);    src2I(JALR_TARGET);  D->decInfo.target = JALR_TARGET;   D->decInfo.is_ret = (rd == 0 && rs1 == 1);  
+          src1I(pc_Plus4);    src2I(JALR_TARGET);  D->decInfo.target = JALR_TARGET;   D->decInfo.is_ret = (rd == 0 && rs1 == 1);  break;
       }
       else{
           src1R(rs1);         src2I(immI(inst));  break;
