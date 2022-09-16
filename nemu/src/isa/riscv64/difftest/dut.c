2e3874369390960e0d518937aee7d5824b74a6ac
1919810 <tracer@ysyx.org>
2022-09-16 13:01:12 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:01:12 up 7 days, 13:25,  3 users,  load average: 0.72, 0.89, 1.00

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -7,6 +7,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   //ref_difftest_regcpy(&temp, DIFFTEST_TO_DUT);
   printf("temp \t ref\n");
   for(int i = 0; i < 32; i++){
+    printf("0x%lx \t 0x%lx\n", cpu.gpr[i], ref_r->gpr[i]);
     if(cpu.gpr[i] ^ ref_r->gpr[i]) return false;
   }
   if(cpu.pc  ^ ref_r -> pc)  return false;
