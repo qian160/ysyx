ceb7c8f3ed3a2a56ea4554af692e1726a2b7c337
1919810 <tracer@ysyx.org>
2022-09-17 00:14:06 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:14:06 up 7 days, 22:58,  3 users,  load average: 0.75, 1.20, 1.17

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -103,7 +103,7 @@ static void load_elf() {
 
     Elf64_Shdr * shdr = (Elf64_Shdr *)(elf_file + shoff);
     char * shstrtab = elf_file + (shdr + shstrndx)->sh_offset;
-    printf("%p\n%p\n", elf_file, shstrtab);
+    printf("%p\n%p\n%p\n", elf_file, shdr, shstrtab);
     char * strtab = NULL;
     Elf64_Sym * symtab = NULL;
     //find the strtab and symtab
