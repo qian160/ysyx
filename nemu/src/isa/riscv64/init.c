11c83b860191cef0ae81b423e6cf18c48e754b84
1919810 <tracer@ysyx.org>
2022-09-17 10:39:04 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:39:04 up 8 days,  1:09,  3 users,  load average: 1.21, 0.81, 0.73

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -7,9 +7,7 @@
 static const uint32_t img [] = {             //offset
   0x00000297,  // auipc t0,0
   0x0002b823,  // sd  zero,16(t0)
-  0x0000006f,  // j 8
   0x00108093,  // addi x1, x1, 1
-  0x0000006f,  // j -8
   0x00108093,  // addi x1, x1, 1
   0x0102b503,  // ld  a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
