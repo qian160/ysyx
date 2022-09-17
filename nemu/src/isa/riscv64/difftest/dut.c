b494bc42242f3ac0bd0c22b30ba76b527fbf21b3
1919810 <tracer@ysyx.org>
2022-09-17 14:56:31 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:56:31 up 8 days,  4:29,  3 users,  load average: 1.53, 1.17, 0.90

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -5,7 +5,10 @@
 //CPU_state and suruct diff_context has same fields
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 32; i++){
-    if(cpu.gpr[i] ^ ref_r->gpr[i]) return false;
+    if(cpu.gpr[i] ^ ref_r->gpr[i]) {
+      Log("\n %s should be 0x%lx, but get 0x%lx\n", reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
+      return false;
+    }
   }
   //printf("\n\ncpu.pc: 0x%lx \t ref.pc: 0x%lx\n", cpu.pc, ref_r->pc);
   if(cpu.pc  ^ ref_r -> pc)  return false;
