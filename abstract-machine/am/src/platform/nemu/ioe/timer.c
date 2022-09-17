5df51b6c89bc218c6710abd977079c4756c12357
1919810 <tracer@ysyx.org>
2022-09-17 10:13:32 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:13:32 up 8 days, 44 min,  3 users,  load average: 1.03, 1.00, 0.96

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,14 +1,17 @@
 #include <am.h>
 #include <nemu.h>
-void __am_timer_init() {
 
+uint64_t init_time = 0;
+
+void __am_timer_init() {
+  init_time = io_read(AM_TIMER_UPTIME).us;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   //uint64_t time;
   //ioe_read(AM_TIMER_UPTIME, &time);
   //io_read(AM_TIMER_UPTIME);
-  uptime->us = io_read(AM_TIMER_UPTIME).us % 1000;
+  uptime->us = io_read(AM_TIMER_UPTIME).us - init_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
