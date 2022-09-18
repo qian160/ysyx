7049f4bed58f08730e5757db3cc36c1546c51f8f
1919810 <tracer@ysyx.org>
2022-09-18 19:25:02 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:02 up  1:06,  1 user,  load average: 1.52, 0.79, 0.63

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -27,7 +27,6 @@ static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
 static void init_screen() {
-  /*
   SDL_Window *window = NULL;
   char title[128];
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
@@ -39,7 +38,6 @@ static void init_screen() {
   SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
-  */
 }
 
 static inline void update_screen() {
