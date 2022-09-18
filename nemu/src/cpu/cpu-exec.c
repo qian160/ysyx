d918b60d14c22ec879ae91ead60556b1b564a463
1919810 <tracer@ysyx.org>
2022-09-18 20:09:46 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:09:46 up  1:50,  1 user,  load average: 1.31, 0.80, 0.61

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -117,7 +117,7 @@ void cpu_exec(uint64_t n) {
 
   uint64_t timer_start = get_time();
   execute(n);
-  show_ftrace();
+  IFDEF(CONFIG_FTRACE_ENABLE, show_ftrace());
 
   uint64_t timer_end = get_time();
   g_timer += timer_end - timer_start;
