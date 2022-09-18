4371c1f96fcf76fb1c3d3219ab875d865bc9b4ac
1919810 <tracer@ysyx.org>
2022-09-18 21:02:48 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.13.0-52-generic #59~20.04.1-Ubuntu SMP Thu Jun 16 21:21:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:02:48 up  2:44,  1 user,  load average: 0.49, 1.13, 1.16

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -20,7 +20,7 @@ void __am_timer_init() {
   uint32_t lo = inl(RTC_ADDR);
   //init_time = read_timer();
   init_time = ((uint64_t)hi << 32) | (uint64_t)lo;
-  printf("init_hi = %x, init_lo = %x, uptime = %d\n", hi, lo, (uint32_t)lo - (uint32_t)init_time );
+  printf("init_hi = %x, init_lo = %x\n", hi, lo);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
@@ -28,7 +28,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t now_lo = inl(RTC_ADDR);
   uint64_t now = ((uint64_t)now_hi << 32) | (uint64_t)now_lo;
   uptime->us = now - init_time;
-  printf("now_hi = %x, now_lo = %x\n", now_hi, now_lo);
+  printf("now_hi = %x, now_lo = %x, uptime = %x\n", now_hi, now_lo, (uint32_t)now_lo - (uint32_t)init_time);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
