391d8e6bd342d3069fa12d9e6bca6b20b432dc91
1919810 <tracer@ysyx.org>
2022-09-20 00:52:49 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:52:49 up  9:19,  1 user,  load average: 0.34, 0.49, 0.44

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -1,14 +1,10 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
-#include "../../../monitor/sdb/cmd.h"
 //CPU_state and suruct diff_context has same fields
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 32; i++){
     if(cpu.gpr[i] ^ ref_r->gpr[i]) {
-      char * t = "d 1";
-      char * arg = strtok(t, " ");
-      cmd_d(arg);
       Log_Color(RED, "\n %s should be 0x%lx, but get 0x%lx\n", reg_name(i), ref_r->gpr[i], cpu.gpr[i]);
       Log_Color(RED, "AT PC = 0X%lx\n", cpu.pc);
       return false;
