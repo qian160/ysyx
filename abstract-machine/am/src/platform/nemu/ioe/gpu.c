5514b5e3b00078e415ed0bee861ac6e673ad214b
1919810 <tracer@ysyx.org>
2022-09-20 19:25:30 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:30 up 18:09,  1 user,  load average: 0.40, 0.46, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,7 +11,7 @@ void __am_gpu_init() {
   int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
   int h = inw(VGACTL_ADDR);      // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = rand();
+  for (i = 0; i < w * h; i ++) fb[i] = rand() * rand();
   outl(SYNC_ADDR, 1);
 }
 
@@ -27,6 +27,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 114514);    //write to SYNC reg will call update
   }
+  //this consumes lots of computations......
   /*
   //write to vga frame buffer
   uint32_t* fb = (uint32_t *)(uintptr_t)FB_ADDR;
@@ -38,7 +39,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     memcpy(&fb[ctl->x + (ctl->y + i) * W], pixels, sizeof(uint32_t) * ctl->w);
     pixels += ctl->w;
   }
-*/
+  */
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
