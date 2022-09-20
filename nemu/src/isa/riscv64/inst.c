42b00f6df075b7d4206958c26f4bae5e4d43b069
1919810 <tracer@ysyx.org>
2022-09-20 09:50:33 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:50:33 up 11:03,  1 user,  load average: 0.87, 0.62, 0.59

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -213,8 +213,8 @@ static int decode_exec(Decode *D) {
       switch(fct7){
         case(0x00):{
           switch(fct3){
-            case(0x00):   R(rd) = SEXT((int)R(rs1) + (int)R(rs2), 32);    break;      //addw
-            case(0x01):   R(rd) = SEXT(R(rs1) << BITS(R(rs2), 4, 0), 32); break;      //sllw
+            case(0x00):   R(rd) = SEXT((int32_t) R(rs1) +  (int32_t) R(rs2), 32);             break;      //addw
+            case(0x01):   R(rd) = SEXT((uint32_t)R(rs1) << (uint32_t)BITS(R(rs2), 4, 0), 32); break;      //sllw
             case(0x05):   R(rd) = SEXT((uint32_t)R(rs1) >> (uint32_t)BITS(R(rs2), 4, 0), 32); break;      //srlw
             default:    panic("bad inst\n");
           }
