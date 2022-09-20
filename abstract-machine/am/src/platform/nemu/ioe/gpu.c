65d1e0d70be40c555af389b01f319b613b252c1a
1919810 <tracer@ysyx.org>
2022-09-20 19:25:47 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:47 up 18:09,  1 user,  load average: 0.45, 0.47, 0.48

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,7 +11,7 @@ void __am_gpu_init() {
   int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
   int h = inw(VGACTL_ADDR);      // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = rand() * rand();
+  for (i = 0; i < w * h; i ++) fb[i] = rand() * rand() * rand();
   outl(SYNC_ADDR, 1);
 }
 
