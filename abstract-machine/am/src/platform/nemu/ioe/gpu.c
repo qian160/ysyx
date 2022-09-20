48f02d11c230d93119103c64b73525ed7fcddafc
1919810 <tracer@ysyx.org>
2022-09-21 00:53:33 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:53:33 up  3:03,  1 user,  load average: 0.82, 0.55, 0.39

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,87 +1,18 @@
 #include <am.h>
 #include <nemu.h>
 #include <klib.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/mman.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<unistd.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
-static int PIXELS[300 * 400];
-
-void load_bmp(){
-
-    int fd = open("/home/s081/code/114514.bmp", O_RDONLY);
-    if(fd == -1){
-        printf("open file %s failed\n", "114514.bmp");
-        exit(1);
-    }
-    //get file size
-    struct stat sb;
-    if(fstat(fd, &sb) == -1){
-        printf("fstat failed\n");
-        exit(1);
-    }
-    size_t size = sb.st_size;
-    //do mmap
-    char * bmp = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if(!bmp){
-        printf("mmap failed\n");
-        exit(1);
-    }
-//    close(fd);
-    //check magic
-    short magic = *(short*)bmp;
-    if(magic ^ 19778){
-        printf("not a bmp file\n");
-        exit(1);
-    }
-    //get header infomation
-    int sz = *(int *)(bmp + 2);
-    int pixel_off         = *(int *)(bmp + 10);
-    u_int32_t width       = *(u_int32_t *)(bmp + 18);
-    u_int32_t height      = *(u_int32_t *)(bmp + 22);
-    u_int32_t color_depth = *(ushort *)(bmp + 28);
-
-    int * pixels = (int *)(bmp + pixel_off);
-    
-    printf("sz = %d, off = %d, width = %d, height = %d, depth = %d\n", sz, pixel_off, width, height, color_depth);
-
-    int stride = (24*width+31)/8;
-    stride = stride / 4 * 4;
-    printf("%d\n", stride);
-    printf("w = %d, h = %d\n", width, height);
-
-    int i, j;
-
-    for(int i =0; i < width * height; i++){
-        int pixel = *pixels;
-        /*
-        int r = (pixel & 0x11000000) >> 24;
-        int g = (pixel & 0x00110000) >> 16;
-        int b = (pixel & 0x00001100) >> 8;
-        printf("%p:    %d, %d, %d\n", pixels, r, g, b);
-        */
-        pixels++;
-        PIXELS[i] = pixel;
-    }
-    
-
-}
-
 
 void __am_gpu_init() {
 
   //TODO: add a photo
-  load_bmp();
+  //load_bmp();
   int i;
   int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
   int h = inw(VGACTL_ADDR);      // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = PIXELS[i];
+  for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 
 }
