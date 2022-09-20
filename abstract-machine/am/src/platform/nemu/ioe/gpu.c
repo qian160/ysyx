d1121c3c16fbc0e7ea3b3fc1c53d3082e81081eb
1919810 <tracer@ysyx.org>
2022-09-21 00:42:14 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:42:14 up  2:52,  1 user,  load average: 0.23, 0.32, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -7,12 +7,29 @@
 void __am_gpu_init() {
 
   //TODO: add a photo
+  /*
   int i;
   int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
   int h = inw(VGACTL_ADDR);      // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
+  */
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  fb[0] = 0x20221d;
+  fb[1] = 0x221dff;
+  fb[2] = 0x1dff20;
+  fb[3] = 0xff2022;
+  fb[4] = 0x20221c;
+  fb[5] = 0x1cff20;
+  fb[6] = 0xff2022;
+  fb[7] = 0x1f221d;
+  fb[8] = 0x231dff;
+  fb[9] = 0x1dff1f;
+  fb[10] = 0xff1f23;
+  fb[11] = 0x1e221d;
+  fb[12] = 0x221dff;
+  fb[13] = 0x1dff1e;
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -27,6 +44,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 114514);    //write to SYNC reg will call update
   }
+  //TODO: improve the performance
   /*
   //this consumes lots of computations......
   //write to vga frame buffer
