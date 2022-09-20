611bc3a84947ba6d3c053adb1d0bbb6eb3918586
1919810 <tracer@ysyx.org>
2022-09-20 16:31:27 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:31:27 up 17:44,  1 user,  load average: 0.13, 0.42, 0.54

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -55,14 +55,9 @@ static inline void update_screen() {
 #endif
 #endif
 
-//user program will call this through io_write
 void vga_update_screen() {
-  uint32_t sync = vgactl_port_base[1];
-  if(sync){
-    update_screen();
-    vgactl_port_base[1] = 0;
-  }
-  
+  // TODO: call `update_screen()` when the sync register is non-zero,
+  // then zero out the sync register
 }
 
 void init_vga() {
