0019dd8583502743aaf73a4c6021595a17bae971
nakashima <1601315694@qq.com>
2022-09-20 16:32:25 +0800

    vga

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -56,8 +56,11 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+  uint32_t sync = vgactl_port_base[1];
+  if (sync != 0) {
+    update_screen();
+    vgactl_port_base[1] = 0;
+  }
 }
 
 void init_vga() {
