27dfcb1ada39b6152111e3c379cf5f2ce5502489
1919810 <tracer@ysyx.org>
2022-09-21 09:59:47 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:59:47 up  4:36,  1 user,  load average: 0.49, 0.36, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -41,7 +41,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
   //int W = inw(VGACTL_ADDR + 2);
   int W = 400;
-  if(ctl -> h == 0 || ctl -> w == 0)  return;
+  //if(ctl -> h == 0 || ctl -> w == 0)  return;
   for (int i = 0; i < ctl -> h; ++i) {
     memcpy(&fb[ctl -> x + (ctl -> y + i) * W], pixels, sizeof(uint32_t) * ctl -> w);
     pixels += ctl->w;
