2d9d2d9b28f892688ffb607f670ddeb38e15e9c6
1919810 <tracer@ysyx.org>
2022-09-21 09:05:11 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:05:11 up  3:42,  1 user,  load average: 0.77, 0.42, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,9 +1,52 @@
 #include <am.h>
 #include <nemu.h>
 #include <klib.h>
+#include <stdio.h>
+//#include<iostream>
+//using namespace std;
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+static int PIXELS[300 * 400];
+/*
+void load_bmp()
+{
+    FILE* f = fopen("/home/s081/code/114514.bmp", "rb");
 
+    if(f == NULL)
+    return ;
+        //throw "Argument Exception";
+
+    unsigned char info[54];
+    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+
+    // extract image height and width from header
+    int width = *(int*)&info[18];
+    int height = *(int*)&info[22];
+
+    int row_padded = (width*3 + 3) & (~3);
+    unsigned char data [row_padded];
+    unsigned char tmp;
+
+    int t = 0;
+    for(int i = 0; i < height; i++)
+    {
+        fread(data, sizeof(unsigned char), row_padded, f);
+        for(int j = 0; j < width*3; j += 3)
+        {
+            // Convert (B, G, R) to (R, G, B)
+            tmp = data[j];
+            data[j] = data[j+2];
+            data[j+2] = tmp;
+            PIXELS[t++] = (data[i] << 16 | data[i+1] << 8) | data[i+2];
+            //printf("R: %d G:%d B%d\n",(int)data[i], (int)data[i+1], (int)data[i+2]);
+//            cout << "R: "<< (int)data[j] << " G: " << (int)data[j+1]<< " B: " << (int)data[j+2]<< endl;
+        }
+    }
+
+    fclose(f);
+    return;
+}
+*/
 void __am_gpu_init() {
 
   //TODO: add a photo
@@ -12,7 +55,7 @@ void __am_gpu_init() {
   int w = inw(VGACTL_ADDR + 2);  // TODO: get the correct width
   int h = inw(VGACTL_ADDR);      // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = 0x114514;
+  for (i = 0; i < w * h; i ++) fb[i] = PIXELS[i];
   outl(SYNC_ADDR, 1);
 
 }
@@ -26,11 +69,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  /*
   if (ctl->sync) {
     outl(SYNC_ADDR, 114514);    //write to SYNC reg will call update
   }
   //TODO: improve the performance
-  /*
+  
   //this consumes lots of computations......
   //write to vga frame buffer
   uint32_t* fb = (uint32_t *)(uintptr_t)FB_ADDR;
