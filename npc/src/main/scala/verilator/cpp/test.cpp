d0115751bc254270e655fc51ae4380387406d943
nakashima <1601315694@qq.com>
2022-09-25 13:24:05 +0800

    template

--- npc/src/main/scala/verilator/cpp/test.cpp
+++ npc/src/main/scala/verilator/cpp/test.cpp
@@ -1,101 +1,22 @@
 #include"debug.h"
+#include"testbench.h"
 using namespace std;
 
-#define EBREAK 0x100073
-
-bool check_bound(uint64_t addr);
-
 VTOP * top = new VTOP("top");
-//VerilatedVcdC * tfp = new VerilatedVcdC;
 vluint64_t TIME = 0;
 static char * img_file = (char *)0;
-
-uint32_t mem[0x80000];	//seems that it can't be too large....
-
-uint32_t readl(uint64_t addr)
-{
-	return mem[(addr - 0x80000000) >> 2];
-}
-
-static size_t load_img()
-{
-	if(!img_file){
-		ifstream in;
-		in.open("./asm/inst_rom");
-		if(in.fail()){
-			cout << "can't open inst rom" << endl;
-			exit(114514);
-		}
-		int n = 0;
-		while(!in.eof())
-			in >> hex >> mem[n++];
-		in.close();
-		return 4096;
-	}
-	cout << img_file << endl;
-	FILE * fp = fopen(img_file, "rb");
-	assert(fp);
-
-	fseek(fp, 0, SEEK_END);
-	size_t size = ftell(fp);
-
-	cout << "the img file is " << img_file << ", size = " << size << endl;
-
-	fseek(fp, 0, SEEK_SET);
-	int ret = fread(mem, size, 1, fp);
-	assert(ret == 1);
-
-	fclose(fp);
-	return size;
-}
-
-void step()
-{
-	//settle all the combinational logic before the edge comes
-	top -> clock = 0;
-	top -> eval();
-	//rising edge
-	top -> clock = 1;
-	top -> io_inst_i = readl(top -> io_pc_o);
-	top -> eval();
-	//falling edge
-	top -> clock = 0;
-	top -> eval();
-
-	//if(top -> clock){	//at rising edge
-	if(Verilated::gotFinish())
-	{
-		cout<<Yellow("ebreak\n");
-		return;
-	}
-	printf("inst = %08x, pc = 0x%lx, src1 = 0x%lx, src2 = 0x%lx\n", \
-		top -> io_inst_o, top -> io_pc_o, top ->io_src1, top->io_src2);
-	std::cout << "wdata = " << hex << top -> io_o << std::endl;
-}
-
-void init(){
-	top -> clock = 0;
-	top -> reset = 1;
-	top -> eval();		//to remember state
-	top -> clock = 1;
-	top -> eval();		//0 -> 1, rising edge. do the reset
-	top -> reset = 0;
-	top -> clock = 0;
-}
 
 int main(int argc, char **argv)
 {
 	Verilated::commandArgs(argc, argv);
-	if(argc < 2){
+	if(argc < 2)
 		cout << "no image is given, use the default inst rom" << endl;
-	}
 	else
 		img_file = strcat(test_path, strcat(argv[1], "-riscv64-npc.bin"));
-	size_t img_size = load_img();
-	init();
+	TestBench<VTOP> tb(img_file);
+	tb.reset();
 	while(!Verilated::gotFinish())
 	{
-		step();
+		tb.tick();
 	}
 }
-
--- npc/src/main/scala/verilator/cpp/testbench.h
+++ npc/src/main/scala/verilator/cpp/testbench.h
@@ -1,10 +1,30 @@
-template<class Module>	class TestBench {
+#include <verilated_vcd_c.h>
+
+//Verilog or chisel can't read binary file to get the instructions, so 
+//we have to add this feature to our cpp testbench, which lose the compability
+
+template<class Module> class TestBench {
+
+	//private, user don't need to know
+	VerilatedVcdC	*m_trace;
 	unsigned long	m_tickcount;
+	size_t	img_size;
 	Module	*m_core;
 
-	TestBench(void) {
+	uint32_t mem[0x80000];	//seems that it can't be too large....
+	uint64_t inst_fetch(uint64_t pc);
+	size_t load_img(char * img_file);
+
+public:
+	TestBench(char * img_file) {
+		// According to the Verilator spec, you *must* call traceEverOn
+		// before calling any of the tracing functions within Verilator.
+		Verilated::traceEverOn(true);
 		m_core = new Module;
+		m_trace	=	nullptr;
 		m_tickcount = 0l;
+		m_core	->	clock = 0;
+		img_size	= load_img(img_file);
 	}
 
 	virtual ~TestBench(void) {
@@ -13,32 +33,101 @@ template<class Module>	class TestBench {
 	}
 
 	virtual void	reset(void) {
-		m_core->reset = 1;
+		m_core -> reset = 1;
+		m_core -> clock = 0;
+		m_core -> eval();
 		// Make sure any inheritance gets applied
-		this->tick();
-		m_core->reset = 0;
+		m_core -> clock = 1;
+		m_core -> eval();
+		m_core -> reset = 0;
+	}
+
+	// Open/create a trace file
+	virtual	void	opentrace(const char *vcdname) {
+		if (!m_trace) {
+			m_trace = new VerilatedVcdC;
+			m_core  -> trace(m_trace, 99);
+			m_trace -> open(vcdname);
+		}
+	}
+
+	// Close a trace file
+	virtual void	close(void) {
+		if (m_trace) {
+			m_trace	-> close();
+			m_trace = NULL;
+		}
 	}
 
 	virtual void	tick(void) {
-		// Increment our own internal time reference
+		// Make sure the tickcount is greater than zero before we do this
 		m_tickcount++;
 
-		// Make sure any combinatorial logic depending upon
-		// inputs that may have changed before we called tick()
-		// has settled before the rising edge of the clock.
+		//set up all the combinational logic before the rising edge
+		m_core	->	clock = 0;
+		m_core	->	eval();
+
+		if(m_trace) m_trace	-> dump(10*m_tickcount-2);
+
+		// Repeat for the positive edge of the clock
+		m_core	->	clock = 1;
+		m_core  ->	io_inst_i = inst_fetch(m_core -> io_pc_o);
+		m_core	->	eval();
+		if(m_trace) m_trace	-> dump(10*m_tickcount);
+		printf("pc = 0x%8lx, inst = %08x, wdata = 0x%-16lx \n src1 = 0x%-16lx, src2 = 0x%-16lx\n\n",\
+			m_core -> io_pc_o, m_core -> io_inst_o, m_core -> io_o, m_core -> io_src1, m_core -> io_src2
+		);
+		if(Verilated::gotFinish()){
+			return;
+		}
+		// Now the negative edge
 		m_core->clock = 0;
 		m_core->eval();
-
-		// Toggle the clock
-
-		// Rising edge
-		m_core->clock = 1;
-		m_core->eval();
-
-		// Falling edge
-		m_core->clock = 0;
-		m_core->eval();
+		if (m_trace) {
+			m_trace->dump(10*m_tickcount+5);
+			m_trace->flush();
+		}
 	}
-
-	virtual bool	done(void) { return (Verilated::gotFinish()); }
 };
+
+
+//some functions' implementation
+template<class Module> 
+size_t TestBench<Module>::load_img(char * img_file){
+		if(!img_file){
+			//use the default inst rom
+			ifstream in;
+			in.open("./asm/inst_rom");
+			if(in.fail()){
+				cout << "can't open inst rom" << endl;
+				exit(114514);
+			}
+			int n = 0;
+			while(!in.eof()){
+				in >> hex >> mem[n++];
+			}
+			cout << "load success" << endl;
+			in.close();
+			if(n) n--;		//cut '\n'
+			return n * sizeof(uint32_t);
+		}
+		FILE * fp = fopen(img_file, "rb");
+		assert(fp);
+
+		fseek(fp, 0, SEEK_END);
+		size_t size = ftell(fp);
+
+		cout << "the img file is " << img_file << ", size = " << size << endl;
+
+		fseek(fp, 0, SEEK_SET);
+		int ret = fread(mem, size, 1, fp);
+		assert(ret == 1);
+
+		fclose(fp);
+		return size;
+}
+
+template<class Module> 
+uint64_t TestBench<Module>::inst_fetch(uint64_t pc){
+	return mem[(pc - 0x80000000) >> 2];
+}
