c4dfce5f70d3fefe2459a9b476d740a614ad44e7
1919810 <tracer@ysyx.org>
2022-10-07 17:51:57 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:51:57 up 2 days,  1:46,  1 user,  load average: 0.46, 0.36, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -3,22 +3,16 @@
 
 static uint64_t init_time = 0;
 
-static inline uint64_t read_timer(){
-  uint64_t hi = ((uint64_t)inl(RTC_ADDR + 4) << 32);
-  uint64_t lo = (uint64_t)inl(RTC_ADDR);
-  return hi | lo;
-}
-
 void __am_timer_init() {
   // the 'inl' will be compiled to 'lw' in asm, remember how lw is implemented in nemu(inst.c):
   // firstly it calls paddr_read. And then pmem_read will find that the address is not in pmem
   // then it will try mmio_read and map_read. After map_read, the call_back function is also called
   // only at offset 4 will the clock be updated 
-  uint32_t hi = inl(RTC_ADDR + 4);    //trigger the handler function
-  uint32_t lo = inl(RTC_ADDR);
-  //init_time = read_timer();
-  init_time = ((uint64_t)hi << 32) | (uint64_t)lo;
-  printf("am_timer_init called\n");
+
+  //uint32_t hi = inl(RTC_ADDR + 4);    //trigger the handler function
+  //uint32_t lo = inl(RTC_ADDR);
+  //init_time = ((uint64_t)hi << 32) | (uint64_t)lo;
+  init_time = ind(RTC_ADDR);
 }
 
 /*  difference between this function and 'rtc_io_handler':
@@ -27,11 +21,11 @@ void __am_timer_init() {
         * so this am function will also trigger 'rtc_io_handler' since it wants to r/w that address
 */
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t now_hi = inl(RTC_ADDR + 4);  //offset = 4 will update the time before reading
-  uint32_t now_lo = inl(RTC_ADDR);
-  uint64_t now = ((uint64_t)now_hi << 32) | (uint64_t)now_lo;
+  //uint32_t now_hi = inl(RTC_ADDR + 4);  //offset = 4 will update the time before reading
+  //uint32_t now_lo = inl(RTC_ADDR);
+  uint64_t now = ind(RTC_ADDR);
+  //uint64_t now = ((uint64_t)now_hi << 32) | (uint64_t)now_lo;
   uptime -> us = now - init_time;
-  printf("am_timer_uptime called\n");
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -2,7 +2,7 @@
 #include <device/alarm.h>
 #include <utils.h>
 
-static uint32_t *rtc_port_base = NULL;
+static uint64_t *rtc_port_base = NULL;
 
 volatile uint64_t npc_timer = 0l;
 
@@ -16,11 +16,12 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   // the attempt to write won't update the timer(execuate the code below)
   // Instead, nothing will happen if we try to write to rtc
   // only read will update the timer
-  if (!is_write && offset == 4) {
+  if (!is_write/* && offset == 4*/) {
     uint64_t us = get_time();
     //rtc just accumulate the time. It's uptime's job to calculate the past time
-    rtc_port_base[0] = (uint32_t)us;    //us
-    rtc_port_base[1] = us >> 32;        //sec
+    //rtc_port_base[0] = (uint32_t)us;    //us
+    //rtc_port_base[1] = us >> 32;        //sec
+    rtc_port_base[0] = us;
     npc_timer = us;
     printf("new timer: 0x%lx\n", us);
   }
@@ -36,7 +37,7 @@ static void timer_intr() {
 #endif
 
 void init_timer() {
-  rtc_port_base = (uint32_t *)new_space(8);
+  rtc_port_base = (uint64_t *)new_space(8);
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
