8d7ba93b4f101078e740e57735d36b7a9f6a467a
1919810 <tracer@ysyx.org>
2022-10-07 13:53:22 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:53:22 up 1 day, 21:48,  1 user,  load average: 0.68, 0.55, 0.52

--- npc/src/main/scala/verilator/cpp/difftest.h
+++ npc/src/main/scala/verilator/cpp/difftest.h
@@ -1,11 +1,11 @@
 #include<iostream>
 //an interesting thing about difftest: if both dut and ref try to print something, then that will be printed twice
-enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
+static enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 #include<dlfcn.h>
-bool (*difftest_checkregs)(void) = nullptr;
-void (*difftest_regcpy)(void *dut, bool direction) = nullptr;
-void (*difftest_exec)(void) = nullptr;
-void (*difftest_init)(char *img_file) = nullptr;
+static bool (*difftest_checkregs)(void) = nullptr;
+static void (*difftest_regcpy)(void *dut, bool direction) = nullptr;
+static void (*difftest_exec)(void) = nullptr;
+static void (*difftest_init)(char *img_file) = nullptr;
 
 uint64_t* npc_timer = 0;
 
