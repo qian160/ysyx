cd89c12d8beece3a724745f9dd4b1d4befb39e6e
1919810 <tracer@ysyx.org>
2022-10-10 20:59:43 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:43 up 3 days, 10:43,  1 user,  load average: 0.54, 0.54, 0.42

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -17,9 +17,9 @@ void isa_reg_display() {
   puts(ANSI_FMT("--------------------------------------------------------------------\n", ANSI_FG_YELLOW));
   putchar('\n');
 
-  printf(ANSI_FMT("mtvec:   0x%lx\n", ANSI_FG_PINK), cpu.csr[MTVEC]);
-  printf(ANSI_FMT("mstatus: 0x%lx\n", ANSI_FG_PINK), cpu.csr[MSTATUS]);
-  printf(ANSI_FMT("mcause:  0x%lx\n", ANSI_FG_PINK), cpu.csr[MCAUSE]);
+  printf(ANSI_FMT("mtvec:   0x%-16lx\n", ANSI_FG_PINK), cpu.csr[MTVEC]);
+  printf(ANSI_FMT("mstatus: 0x%-16lx\n", ANSI_FG_PINK), cpu.csr[MSTATUS]);
+  printf(ANSI_FMT("mcause:  0x%-16lx\n", ANSI_FG_PINK), cpu.csr[MCAUSE]);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
