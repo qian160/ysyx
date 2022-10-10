d7b4aab3a197112cd4d59264af8e6f0a4789bc16
1919810 <tracer@ysyx.org>
2022-10-10 23:49:18 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:49:18 up 3 days, 13:33,  1 user,  load average: 0.35, 0.24, 0.22

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,6 +11,7 @@ Context* __am_irq_handle(Context *c) {
 //  asm volatile ("j 0");
   if (user_handler) {
     Event ev = {0};
+    c -> mepc += 4;
     switch (syscall_num) {
       case -1: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
