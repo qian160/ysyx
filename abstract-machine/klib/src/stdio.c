ad17999088182340910fb6710ee43d83eb80bd22
1919810 <tracer@ysyx.org>
2022-10-10 15:36:34 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:34 up 3 days,  6:33,  1 user,  load average: 0.29, 0.24, 0.35

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -56,6 +56,7 @@ char *itoa(int number, int base)  //10, 16
 }
 
 int printf(const char *fmt, ...) {
+  /*
   va_list ap;
   va_start(ap, fmt);
   char buf[1024];
@@ -65,6 +66,8 @@ int printf(const char *fmt, ...) {
     putch(buf[i]);
   }
   return n;
+  */
+  return 0;
 }
 
 //v: use va_list as argument instead of ... its behavior is same as sprintf
