37c1fb8c1040f31af4b02c9dad1be3e222e294a5
1919810 <tracer@ysyx.org>
2022-10-10 14:15:56 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:15:56 up 3 days,  5:12,  1 user,  load average: 0.90, 0.78, 0.50

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -10,7 +10,6 @@ void init_proc(void);
 int main() {
   extern const char logo[];
   printf("%s", logo);
-  printf("test\n");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
