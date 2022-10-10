4ac0d562f5d1e85c25fa7e09c2de5c57981aa8a9
1919810 <tracer@ysyx.org>
2022-10-10 21:50:35 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:50:35 up 3 days, 11:34,  1 user,  load average: 0.88, 0.76, 0.56

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -2,7 +2,6 @@
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
-#include "../../../../abstract-machine/am/include/am.h"
 #include "../../../include/generated/autoconf.h"
 #include "../../../include/trace.h"   //load op will set ringbuf's rd
 
@@ -261,7 +260,7 @@ static int decode_exec(Decode *D) {
       switch(fct3){
         case(0):{
           switch(immI(inst)){
-            case(0):  isa_raise_intr(EVENT_ECALL, D->pc);break;  // ecall
+            case(0):  isa_raise_intr(0, D->pc);break;  // ecall
             case(1):  NEMUTRAP(D->pc, R(10));     break;  // ebreak
             default:  panic("bad sys inst\n");    break;
           }
