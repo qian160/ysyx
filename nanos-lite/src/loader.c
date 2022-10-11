e0cbb4c627c01495bc901b94066ffb6a46824211
1919810 <tracer@ysyx.org>
2022-10-11 13:48:49 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:48:49 up 3 days, 18:30,  1 user,  load average: 0.50, 0.50, 0.46

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -51,9 +51,9 @@ Start of section headers:\t\t%u\n---------------------------------------------\n
 
     if(pgm_header.p_type == PT_LOAD) {
 
-      printf("---------------------------------------------\n\n\
+      printf("---------------------------------------------\n\
 Program Headers:\n Offset:\t%08x\n VirtAddr:\t%p\n FileSiz:\t%08x\n MemSiz:\t%08x\n\
----------------------------------------------\n",\
+---------------------------------------------\n\n",\
         pgm_header.p_offset, pgm_header.p_vaddr, pgm_header.p_filesz, pgm_header.p_memsz);
 
       // read segment and write into memery
