72b318307336e83c2f65050f3e284a139f846ebd
1919810 <tracer@ysyx.org>
2022-10-11 11:18:48 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:18:48 up 3 days, 17:25,  1 user,  load average: 0.80, 0.62, 0.45

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,9 +9,13 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t file_offset;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_RAMDISK};
+
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -25,12 +29,73 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_RAMDISK]= {"/home/s081/Downloads/ysyx-workbench/nanos-lite/build/ramdisk.img",0, 0, ramdisk_read, ramdisk_write},
 #include "files.h"
 };
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+#define total_file_num sizeof(file_table)/sizeof(Finfo)
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence){
+  case SEEK_SET:
+    // assert(file_table[fd].size >= offset);
+    file_table[fd].file_offset = offset;
+    break;
+
+  case SEEK_CUR:
+    file_table[fd].file_offset += offset;
+    // assert(file_table[fd].file_offset <= file_table[fd].size);
+    break;
+
+  case SEEK_END:
+    file_table[fd].file_offset = file_table[fd].size + offset;
+    break;
+
+  default: assert(0);
+  }
+
+  return file_table[fd].file_offset;
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for(int fd = 0; fd < total_file_num; ++fd)
+    if(strcmp(pathname, file_table[fd].name)==0) {
+      file_table[fd].file_offset = 0;
+      if(fd > 2) {
+        file_table[fd].read  = ramdisk_read;
+        file_table[fd].write = ramdisk_write;
+      }
+// printf("----------------\n[fs_open]\n file %d: \"%s\"\n size: %p\n----------------\n", fd, pathname, file_table[fd].size);
+      return fd;
+    }
+
+  assert(0);
+  return 114514;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  if(fd <= 2) return 0; // ignore stdin, stdout and stderr
+
+// printf("----------------\n[fs_read] fd %d\n from (%u + offset %d)\n size=%p\n----------------\n",
+// fd, file_table[fd].disk_offset, file_table[fd].file_offset, len);
+
+  file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
+  fs_lseek(fd, len, SEEK_CUR);
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
+  return fs_lseek(fd, len, SEEK_CUR);
+}
+
+int fs_close(int fd) {
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,9 +10,66 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern size_t fs_write(int fd, const void *buf, size_t len);
+extern size_t fs_lseek(int fd, size_t offset, int whence);
+extern int fs_close(int fd);
+
+static Elf_Ehdr elf_header;
+static Elf_Phdr pgm_header;
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  assert(filename);
+  // read ELF header
+  int elf_fd = fs_open(filename,0,0);
+  printf("loader: hello fd=%d\n", elf_fd);
+  fs_read(elf_fd, &elf_header, sizeof(Elf_Ehdr));
+
+  printf("---------------------------------------------\n\
+  ELF Header:\n Entry point address:\t\t%p\n Start of program headers:\t\t%u\n\
+  Start of section headers:\t\t%u\n---------------------------------------------\n",\
+  elf_header.e_entry, elf_header.e_phoff, elf_header.e_shoff);
+
+  // check elf magic number           0x7f, 'E, 'L', 'F'(LSB TO MSB)
+  assert(*(int*)elf_header.e_ident == 0x464c457f);
+
+  // read and analyze each program header
+  for(int i = 0; i < elf_header.e_phnum; ++i) {
+    fs_lseek(elf_fd, elf_header.e_phoff + i*sizeof(Elf_Phdr), SEEK_SET); // 0+64
+    fs_read(elf_fd, &pgm_header, sizeof(Elf_Phdr));
+
+    if(pgm_header.p_type == PT_LOAD) {
+
+      printf("---------------------------------------------\n\
+        Program Headers:\n Offset:\t0x%08x\n VirtAddr:\t%p\n FileSiz:\t0x%08x\n MemSiz:\t0x%08x\n\
+        ---------------------------------------------\n",\
+        pgm_header.p_offset, pgm_header.p_vaddr, pgm_header.p_filesz, pgm_header.p_memsz);
+
+      // read segment and write into memery
+      // # length  : pgm_header.p_memsz Bytes
+      // from elf  
+      // # start at: pgm_header.p_offset
+      // to memery
+      // # start at: pgm_header.p_vaddr
+      fs_lseek(elf_fd, pgm_header.p_offset, SEEK_SET);
+      fs_read (elf_fd, (void*)pgm_header.p_vaddr,  pgm_header.p_memsz);
+
+      // set segment '.bss' to zero 
+      // # length  : pgm_header.p_memsz  - pgm_header.p_filesz
+      // to memery
+      // # start at: pgm_header.p_vaddr  + pgm_header.p_filesz
+      // to elf
+      // # start at: pgm_header.p_offset + pgm_header.p_filesz
+      memset((void*)(pgm_header.p_vaddr + pgm_header.p_filesz), 0, pgm_header.p_memsz - pgm_header.p_filesz);
+    }
+  }
+  // fs_close(elf_fd);
+  return elf_header.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
