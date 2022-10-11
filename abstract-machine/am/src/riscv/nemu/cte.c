ef375ee58599bef0b5a1a1d99dac9d6ec5823841
1919810 <tracer@ysyx.org>
2022-10-11 20:05:32 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:05:32 up 3 days, 22:56,  3 users,  load average: 3.45, 2.85, 2.49

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,9 +14,8 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     printf("call number: %d\n", syscall_num);
     c -> mepc += 4;   //need further consideration
-    if(syscall_num >= SYS_exit && syscall_num <= SYS_gettimeofday){
+    if(syscall_num >= SYS_exit && syscall_num <= SYS_gettimeofday)
       ev.event = EVENT_SYSCALL; //do_event will figure out the syscall 
-    }
     else if(syscall_num == -1)
       ev.event  = EVENT_YIELD;
     else{
