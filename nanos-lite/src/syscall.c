a98e1365fc51f63acdb7d06c6f891eada3f7effa
1919810 <tracer@ysyx.org>
2022-10-11 15:53:58 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:58 up 3 days, 20:35,  1 user,  load average: 0.83, 0.69, 0.55

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,16 +11,16 @@ extern size_t fs_lseek(int fd, size_t offset, int whence);
 void do_syscall(Context *c) {
   uintptr_t a7 = c -> CALL_NUMBER;
   
-  uintptr_t _a0 = c -> SYSCALL_ARG1;
-  uintptr_t _a1 = c -> SYSCALL_ARG2;
-  uintptr_t _a2 = c -> SYSCALL_ARG3;
+  uintptr_t _a0  __attribute__((unused))= c -> SYSCALL_ARG1;
+  uintptr_t _a1  __attribute__((unused))= c -> SYSCALL_ARG2;
+  uintptr_t _a2  __attribute__((unused))= c -> SYSCALL_ARG3;
   uintptr_t _ret __attribute__((unused));
 
   switch (a7) {
     case SYS_yield:  yield();   break;
     case SYS_exit:  halt(_a0);  break;
     //case SYS_read:
-    case SYS_write: _ret = fs_write(_a0, (char *)_a1, _a2); break;
+    //case SYS_write: _ret = fs_write(_a0, (char *)_a1, _a2); break;
 
     default: panic("Unhandled syscall ID = %d", a7);
   }
