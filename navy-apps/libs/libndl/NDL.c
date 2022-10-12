8ebcc0ae64278cdfd1d0c584d376d872b88da66c
1919810 <tracer@ysyx.org>
2022-10-13 01:10:33 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:10:33 up 4 days, 13:53,  3 users,  load average: 1.70, 1.47, 1.43

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -18,9 +18,8 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  int fd = open("/dev/events", O_RDONLY);
-  return read(fd, buf, len);
-  return 0;
+  int fd = _open("/dev/events", O_RDONLY);
+  return _read(fd, buf, len);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
