1cc5b461c78144fff95b991f06ea71203c380bf6
1919810 <tracer@ysyx.org>
2022-10-12 18:43:44 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:43:44 up 4 days,  7:26,  3 users,  load average: 1.50, 1.32, 1.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,17 +28,17 @@ size_t write(uint32_t fd, char *buf, size_t len) {
 
 void do_syscall(Context *c) {
   uintptr_t a7 = c -> CALL_NUMBER;
-  uintptr_t _a0  __attribute__((unused)) = c -> SYSCALL_ARG1;
-  uintptr_t _a1  __attribute__((unused)) = c -> SYSCALL_ARG2;
-  uintptr_t _a2  __attribute__((unused)) = c -> SYSCALL_ARG3;
-  uintptr_t _ret __attribute__((unused)) = c -> SYSCALL_RETV;
+  uintptr_t  _a0  __attribute__((unused)) = c -> SYSCALL_ARG1;
+  uintptr_t  _a1  __attribute__((unused)) = c -> SYSCALL_ARG2;
+  uintptr_t  _a2  __attribute__((unused)) = c -> SYSCALL_ARG3;
+  uintptr_t* _ret __attribute__((unused)) = &c -> SYSCALL_RETV;
 
   switch (a7) {
     case SYS_yield:   yield();    break;
     case SYS_exit:    halt(_a0);  break;
     //case SYS_read:
-    case SYS_write:   _ret = write(_a0, (char *)_a1, _a2); break;//fs_write(_a0, (char *)_a1, _a2); break;
-    case SYS_brk:     _ret = 0;   break;
+    case SYS_write:   *_ret = write(_a0, (char *)_a1, _a2); break;//fs_write(_a0, (char *)_a1, _a2); break;
+    case SYS_brk:     *_ret = 0;   break;
 
     default: panic("Unhandled syscall ID = %d", a7);
   }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -43,7 +43,7 @@
 #endif
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
-  register intptr_t _gpr1 asm (GPR1) = type;
+  register intptr_t _gpr1 asm (GPR1) = type;    // a7
   register intptr_t _gpr2 asm (GPR2) = a0;
   register intptr_t _gpr3 asm (GPR3) = a1;
   register intptr_t _gpr4 asm (GPR4) = a2;
