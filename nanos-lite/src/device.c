184fd8d8262515109b7bccc9b4230795fd1c2915
1919810 <tracer@ysyx.org>
2022-10-13 01:29:41 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:29:41 up 4 days, 14:12,  3 users,  load average: 1.50, 1.61, 1.57

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,7 +30,6 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   buf = (char*)buf;
   AM_INPUT_KEYBRD_T key = io_read(AM_INPUT_KEYBRD);
   printf("keycode: %d,  down:%d\n", key.keycode, key.keydown);
-  while(1);
   if(key.keycode == AM_KEY_NONE)  return 0;
 
   char *tag = key.keydown ? "keydown " : "keyup ";
