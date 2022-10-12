78e07f72d9d57d2d960b623dd232402dee0fd7f5
1919810 <tracer@ysyx.org>
2022-10-12 20:43:39 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:43:39 up 4 days,  9:26,  3 users,  load average: 1.45, 1.47, 1.59

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -21,7 +21,10 @@ size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
 }
-
+size_t serial_write2(const void *buf, size_t off, size_t len){
+  putch('1');
+  return 0;
+}
 size_t invalid_write(const void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
@@ -30,8 +33,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write2},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write2},
 #include "files.h"
 };
 
