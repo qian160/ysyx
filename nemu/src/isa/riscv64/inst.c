db480f174a7187c92c28a2bd91630e0efff4bb58
1919810 <tracer@ysyx.org>
2022-10-12 14:04:36 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:36 up 4 days,  3:41,  3 users,  load average: 1.64, 1.42, 1.36

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -263,7 +263,7 @@ static int decode_exec(Decode *D) {
     }
 
     case(JAL):    D->decInfo.type = TYPE_J;    R(rd) = linkAddr; D -> dnpc = D -> pc + immJ(inst);  IFDEF(CONFIG_REF, printf("jal, target at 0x%lx\n", D -> pc + immJ(inst)));    break;
-    case(JALR):   printf("pc = 0x%lx, imm = %ld, target = 0x%lx\n", D-> pc, immI(inst), R(rs1) + immI(inst));D->decInfo.type = TYPE_I;    R(rd) = linkAddr; D -> dnpc = R(rs1) + immI(inst);   IFDEF(CONFIG_REF, printf("jalr, target at 0x%lx\n", R(rs1) + immI(inst)));    break;
+    case(JALR):   D->decInfo.type = TYPE_I;    R(rd) = linkAddr; D -> dnpc = R(rs1) + immI(inst);   IFDEF(CONFIG_REF, printf("jalr, target at 0x%lx\n", R(rs1) + immI(inst)));    break;
     case(AUIPC):  D->decInfo.type = TYPE_U;    R(rd) = D -> pc + immU(inst);break;
     case(LUI):    D->decInfo.type = TYPE_U;    R(rd) = immU(inst);break;
     case(SYS):{
@@ -299,6 +299,7 @@ static int decode_exec(Decode *D) {
   //printf("pc: 0x%lx\n", D->pc);
   R(0) = 0; // reset $zero to 0
   Assert( D-> dnpc != D -> pc, "dead loop at 0x%lx\n", cpu.pc);
+  printf("pc = 0x%lx, dnpc = 0x%lx\n", D->pc, D->dnpc);
   //IFDEF(CONFIG_REF, Log("\nwdata = 0x%lx\npc = 0x%8lx, inst = 0x%08x,  rd = %d\n", opcode == BRANCH? 0: R(rd), D->pc, inst, rd));
   return 0;
 }
