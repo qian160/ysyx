68c4d10817f3f49ebf0c1ea133498640e92561ec
1919810 <tracer@ysyx.org>
2022-10-13 15:26:51 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:26:51 up 4 days, 16:45,  3 users,  load average: 2.09, 1.62, 1.56

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -58,7 +58,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   const uint32_t *src = (uint32_t *)buf;
   uint32_t *fb = (uint32_t *)(uintptr_t)(FB_ADDR + offset); //字节编址
-  for (int i = 0; i < len ; ++i){
+  for (int i = 0; i < len / 4; ++i){
     fb[i] = src[i];
   }
   
