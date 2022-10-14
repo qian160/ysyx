d0fed630ee54147e406dc8cb82773bf704e10205
1919810 <tracer@ysyx.org>
2022-10-14 20:04:06 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:04:06 up 5 days,  4:13,  3 users,  load average: 1.61, 1.63, 1.42

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -93,7 +93,6 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
 
 int SDL_PollEvent(SDL_Event *ev) {
   uint8_t type = 0, sym = 0;
-  printf("SDL_PollEvent\n");
   if (read_keyinfo(&type, &sym)){
     ev->type = type;
     ev->key.keysym.sym = sym;
