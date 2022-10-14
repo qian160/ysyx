3f8f4172ccaf16e77c72382674dd42f5e43748e5
1919810 <tracer@ysyx.org>
2022-10-14 10:47:33 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:47:33 up 4 days, 21:25,  3 users,  load average: 1.10, 1.26, 1.28

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,8 +36,8 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  [FD_EVENT]  = {"/dev/events",  0, 0, events_read,  invalid_write},
   [FD_FB]     = {"/dev/fb",0, 0, invalid_read, fb_write},
+  [FD_EVENT]  = {"/dev/events",  0, 0, events_read,  invalid_write},
   {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
   //ramdisk
 #include "files.h"
