3a3fa8816f523a5f3cec57cd828ae670441685b7
1919810 <tracer@ysyx.org>
2022-10-14 14:20:07 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:20:07 up 4 days, 23:46,  3 users,  load average: 1.85, 1.42, 1.28

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -143,8 +143,10 @@ static void draw_text_row(char *s, int r) {
 
 static void display_menu(int n) {
   clear_display();
+  printf("test\n");
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+  printf("test\n");
   printf("Available applications:\n");
   char buf[80];
   int i;
