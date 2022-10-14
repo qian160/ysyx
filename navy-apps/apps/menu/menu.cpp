eaf24b32e3dbc8ca3a3c60cb3e727a3a92a32cbd
1919810 <tracer@ysyx.org>
2022-10-14 14:11:37 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:11:37 up 4 days, 23:38,  3 users,  load average: 1.49, 1.26, 1.15

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -76,8 +76,8 @@ int main(int argc, char *argv[], char *envp[]) {
   set_i_max();
 
   while (1) {
-	  printf("?");
-    display_menu(i_max);		//bug detacted
+    display_menu(i_max);
+	printf("dis");
     SDL_Event e;
     do {
       SDL_WaitEvent(&e);
@@ -140,9 +140,7 @@ static void draw_text_row(char *s, int r) {
 }
 
 static void display_menu(int n) {
-  printf("1");
   clear_display();
-  printf("1");
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
