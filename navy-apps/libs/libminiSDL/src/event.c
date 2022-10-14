34a55f5114c9f4ea7de788288fb546598009b3da
1919810 <tracer@ysyx.org>
2022-10-14 19:45:58 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:45:58 up 5 days,  3:55,  3 users,  load average: 1.53, 1.30, 1.28

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -62,6 +62,7 @@ static int inline read_keyinfo(uint8_t *type, uint8_t *sym){
     return 0;
   }
   printf("%s\n", key_buf);
+  printf("1\n");
   key_action = key_buf;
   int i;
   for (i = 0; key_buf[i] != ' '; i++){}
