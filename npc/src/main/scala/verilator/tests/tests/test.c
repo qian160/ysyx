babdaf2662d72e2327cf43dea626e501661ba2e4
1919810 <tracer@ysyx.org>
2022-10-17 11:50:47 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:50:47 up 6 days,  7:53,  3 users,  load average: 1.51, 1.37, 1.35

--- npc/src/main/scala/verilator/tests/tests/test.c
+++ npc/src/main/scala/verilator/tests/tests/test.c
@@ -1,8 +1,12 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+/*
 __attribute__((noinline))
 void halt(int code) {
     __asm__ volatile("mv a0, %0; ebreak" : :"r"(code));
 }
-
+*/
 int main()
 {
 	int j = 4;
