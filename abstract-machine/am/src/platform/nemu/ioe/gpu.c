5b2ccadd57af7d68f489d6cdcece424be6e03009
1919810 <tracer@ysyx.org>
2022-10-25 19:15:40 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:15:40 up 1 day, 15:00,  1 user,  load average: 0.74, 0.58, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -88,7 +88,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     bestFunc(&fb[ctl -> x + (ctl -> y + row) * W], pixels, ctl -> w);
     pixels += ctl -> w;
   }
-  outl(SYNC_ADDR, ctl->sync);
+  //outl(SYNC_ADDR, ctl->sync);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
