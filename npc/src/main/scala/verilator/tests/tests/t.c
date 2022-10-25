85ea98c8d95a993453f53ab07c1082a9379926de
1919810 <tracer@ysyx.org>
2022-10-25 21:04:53 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:04:53 up 1 day, 16:50,  1 user,  load average: 0.12, 0.19, 0.26

--- npc/src/main/scala/verilator/tests/tests/t.c
+++ npc/src/main/scala/verilator/tests/tests/t.c
@@ -1,6 +1,8 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
+#include "/home/s081/Downloads/ysyx-workbench/abstract-machine/am/src/riscv/riscv.h"
+
 /*
 __attribute__((noinline))
 void halt(int code) {
@@ -9,7 +11,7 @@ void halt(int code) {
 */
 int main()
 {
-	//int z __attribute__((unused)) = SYNC_ADDR;
+	int z __attribute__((unused)) = SYNC_ADDR;
 	int w __attribute__((unused)) = 1;
 	return 0;
 }
