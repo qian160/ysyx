ebb062772976965d72e103f199c55d6ac6634bd3
nakashima <1601315694@qq.com>
2022-10-27 20:11:40 +0800

    vga

--- abstract-machine/am/include/am.h
+++ abstract-machine/am/include/am.h
@@ -23,9 +23,10 @@ typedef struct Context Context;
 // An event of type @event, caused by @cause of pointer @ref
 typedef struct {
   enum {
+    EVENT_NULL, EVENT_YIELD,
     EVENT_ECALL,
-    EVENT_YIELD, EVENT_SYSCALL, EVENT_PAGEFAULT, EVENT_ERROR,
-    EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,EVENT_NULL,
+    EVENT_SYSCALL, EVENT_PAGEFAULT, EVENT_ERROR,
+    EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,
   } event;
   uintptr_t cause, ref;
   const char *msg;
