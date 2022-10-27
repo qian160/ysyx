169e19d9d4162387c7457039978f713c7262679d
1919810 <tracer@ysyx.org>
2022-10-27 21:08:43 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:08:43 up 2 days, 23:48,  1 user,  load average: 0.65, 0.42, 0.38

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -4,8 +4,8 @@
 //#define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
 //#define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
 
-#define SCREEN_W 400
-#define SCREEN_H 300
+#define SCREEN_W 200
+#define SCREEN_H 150
 
 static uint32_t screen_width() {
   return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).width, SCREEN_W);
