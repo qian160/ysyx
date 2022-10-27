b4d2f0f2797c9e0b6724e61f21d7f1b155df55fc
1919810 <tracer@ysyx.org>
2022-10-28 01:39:38 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:39:38 up 3 days,  4:19,  1 user,  load average: 1.06, 0.77, 0.42

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -26,7 +26,7 @@ static uint32_t *vgactl_port_base = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
-void update_screen();
+static inline void update_screen();
 int vga_auto_update(void *p)
 {
   while(1)
