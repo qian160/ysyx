a71bf46f17934e0e96051757bb2ca890140f706a
1919810 <tracer@ysyx.org>
2022-10-28 01:56:33 +0800

    > compile NEMU 114514 张三 Linux ubuntu 5.15.0-50-generic #56~20.04.1-Ubuntu SMP Tue Sep 27 15:51:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:56:33 up 3 days,  4:36,  1 user,  load average: 0.83, 1.03, 1.06

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -25,22 +25,26 @@ static uint32_t *vgactl_port_base = NULL;
 
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
+/*
+// 要注意，nemu只是个指令集模拟器，并不等于真实硬件，
+// 不管你程序写的怎么样到他手里都是变成单线程一句句来执行的。
+// 享受不到多线程的加成。加入多线程之后反而会影响性能
 
 static inline void update_screen();
 int vga_auto_update(void *p)
 {
   while(1)
   {
-    //static uint64_t last = 0;
-    //uint64_t now = get_time();
-    //if (now - last < 10/*1000000 / 60*/) continue;
-    //last = now;
+    static uint64_t last = 0;
+    uint64_t now = get_time();
+    if (now - last < 1000000 / 60) continue;
+    last = now;
     IFDEF(CONFIG_HAS_VGA, update_screen());
 
   }
     return 114514;
 }
-
+*/
 static void init_screen() {
   SDL_Window *window = NULL;
   char title[128];
@@ -63,7 +67,7 @@ static void init_screen() {
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 
-    SDL_CreateThread(vga_auto_update, "vga auto update", NULL);
+//    SDL_CreateThread(vga_auto_update, "vga auto update", NULL);
 }
 
 //called in device_update
